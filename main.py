52798b5e1f9116ff31c1c6d778c080283ecf5b8f
Moonlight <ademuyiwaidowu47@gmail.com>
2023-04-05 10:53:59 +0100

    Add translator

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from greet import greetings
+from translate import Translator
+
+translator = Translator(to_lang="pt") #initializing translator
 for g in greetings:
-    print(g.title())
+    print(translator.translate(g).title())
 # title will change the first letters in your list
 #to capital letter
+#changing the list to portugese on the console
